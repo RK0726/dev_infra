1408cf53a8a5538c857bcd0015e8453b2d56b6fd
RK0726 <th.iws.soo@gmail.com>
2024-09-06 23:23:57 +0900

    stateをS3に

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "tskt-terraform"
+    region = "ap-northeast-1"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region  = "ap-northeast-1"
 }
