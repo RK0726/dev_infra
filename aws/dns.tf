04a57e21a679d04180b8ca5fd087ee255ffed7eb
RK0726 <th.iws.soo@gmail.com>
2024-09-07 00:19:35 +0900

    aレコード追加

--- aws/dns.tf
+++ aws/dns.tf
@@ -6,3 +6,11 @@ resource "aws_route53_zone" "tskt" {
     AppName = var.APP_NAME
   }
 }
+
+resource "aws_route53_record" "tskt_a" {
+  zone_id = aws_route53_zone.tskt.zone_id
+  name    = "tskt.net"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ec2.public_ip]
+}
