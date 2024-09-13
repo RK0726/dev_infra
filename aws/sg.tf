055bae9c1b4b06563a7188baf25e7db1f88d132c
RK0726 <th.iws.soo@gmail.com>
2024-09-13 19:13:03 +0900

    sg追加

--- aws/sg.tf
+++ aws/sg.tf
@@ -33,6 +33,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
   security_group_id = aws_security_group.web_sg.id
   cidr_ipv4         = "0.0.0.0/0"
