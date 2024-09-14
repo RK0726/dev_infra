f4b965e0eff21fcf9415ce1a695be0ec1f361864
RK0726 <th.iws.soo@gmail.com>
2024-09-14 15:12:49 +0900

    プライベートホストゾーン追加

--- aws/dns.tf
+++ aws/dns.tf
@@ -2,8 +2,7 @@ resource "aws_route53_zone" "tskt" {
   name = "tskt.net"
 
   tags = {
-    Name    = "${var.APP_NAME}-hosted-zone"
-    AppName = var.APP_NAME
+    Name    = "public-hosted-zone"
   }
 }
 
@@ -13,4 +12,24 @@ resource "aws_route53_record" "tskt_a" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.ec2.public_ip]
+}
+
+#### Private DNS ####
+resource "aws_route53_zone" "tskt_private" {
+  name = "internal.tskt.net"
+  vpc {
+    vpc_id = aws_vpc.vpc.id
+  }
+
+  tags = {
+    Name    = "private-hosted-zone"
+  }
+}
+
+resource "aws_route53_record" "tskt_private_a" {
+  zone_id = aws_route53_zone.tskt_private.zone_id
+  name    = "internal.tskt.net"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ec2.private_ip]
 }
